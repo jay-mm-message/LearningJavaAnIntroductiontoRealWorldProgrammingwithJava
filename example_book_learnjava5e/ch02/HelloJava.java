416e69a506fbf56da4eff337a3ada6e9f0eb8f04
jay <jay.mm.message@gmail.com>
2024-03-11 15:47:11 +0800

    Refactor

--- example_book_learnjava5e/ch02/HelloJava.java
+++ example_book_learnjava5e/ch02/HelloJava.java
@@ -6,9 +6,11 @@ import javax.swing.*;
  * A bare bones graphical version of Hello World.
  */
 public class HelloJava {
+    static String szFrame = new String("Hello, Java! frame");
+    static String szLabel = new String("Hello, Java! label");
     public static void main( String[] args ) {
-        JFrame frame = new JFrame( "Hello, Java! frame " );
-        JLabel label = new JLabel("Hello, Java! label ", JLabel.CENTER );
+        JFrame frame = new JFrame(szFrame);
+        JLabel label = new JLabel(szLabel, JLabel.CENTER );
         frame.add(label);
         frame.setSize( 300, 300 );
         frame.setVisible( true );
